e5a3cdf2cb6d3ba121cb40087d9e081d4fb89f73
aixuebo <78608544@qq.com>
2016-05-20 19:23:22 +0800

    update

--- src/main/java/com/maming/common/db/QueyeSqlTest.java
+++ src/main/java/com/maming/common/db/QueyeSqlTest.java
@@ -1,6 +1,7 @@
 package com.maming.common.db;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -14,12 +15,12 @@ public class QueyeSqlTest {
 			Connection commonConnection = connectPool.getCommonConnection();
 			
 			StringBuffer sql = new StringBuffer();
-			//sql.append("select * FROM user_info limit 10");
-			sql.append("SELECT * FROM(");
+			sql.append("select * FROM user_info limit 10");
+/*			sql.append("SELECT * FROM(");
 			sql.append(" SELECT a.*, b.cur_plus_money,b.cur_regular_money,(b.cur_plus_money + b.cur_regular_money) AS premium ");
 			sql.append(" FROM ( SELECT MAX(m1.money_record_id) AS id,m1.userid FROM money_record_additional AS m1 WHERE m1.createDate < '2016-5-11 00:00:00' AND m1.userid IS NOT NULL GROUP BY m1.userId ) AS a");
 			sql.append(" LEFT JOIN money_record_additional AS b ON a.id = b.money_record_id HAVING premium > 0) aa ORDER BY premium DESC,userid");
-			
+		*/	
 			PreparedStatement st = commonConnection.prepareStatement(sql.toString());
 			ResultSet rs = st.executeQuery();
 			
@@ -40,9 +41,29 @@ public class QueyeSqlTest {
 		}
 	}
 	
-	public static void main(String[] args) {
+	public void query2(){
+		try{
+			Connection commonConnection = connectPool.getCommonConnection();
+			DatabaseMetaData dbmd = commonConnection.getMetaData();
+		    String [] tableTypes = { "TABLE" };
+		    ResultSet rs = dbmd.getTables(null, null, null, tableTypes);//获取全部table
+		    if (null != rs) {
+		      try {
+		        while (rs.next()) {
+		          System.out.println(rs.getString("TABLE_NAME"));
+		        }
+		      } finally {
+		        rs.close();
+		        commonConnection.close();
+		      }
+		    }
+		} catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
 	
+	public static void main(String[] args) {
 		QueyeSqlTest test = new QueyeSqlTest();
-		test.query1();
+		test.query2();
 	}
 }
